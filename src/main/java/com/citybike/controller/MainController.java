a5602ee07ebcc7b867bc6feab8e9aff7faadacf9
homebeach <petri.kotiranta@gmail.com>
2023-04-19 22:06:29 +0300

    Added pagination for Journeys

--- src/main/java/com/citybike/controller/MainController.java
+++ src/main/java/com/citybike/controller/MainController.java
@@ -6,6 +6,10 @@ import com.citybike.model.JourneyDTO;
 import com.citybike.model.JourneyWithStationsDTO;
 import com.citybike.model.StationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,14 +31,26 @@ public class MainController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping(path="/journeys", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<JourneyWithStationsDTO>> getJourneys() {
-        List<JourneyDTO> journeyDTOS = (List<JourneyDTO>) journeyRepository.findAll();
-        List<JourneyWithStationsDTO> journeyDTOs = journeyDTOS.stream()
-                .map(journey -> new JourneyWithStationsDTO(journey))
-                .collect(Collectors.toList());
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("total-pages", "1");
-        return new ResponseEntity<>(journeyDTOs, headers, HttpStatus.OK);
+    public ResponseEntity<List<JourneyWithStationsDTO>> getJourneys(@RequestParam(defaultValue = "0") int page,
+                                                                    @RequestParam(defaultValue = "10") int size) {
+        Pageable paging = PageRequest.of(page, size);
+        Page<JourneyDTO> pagedResult = journeyRepository.findAll(paging);
+        if (pagedResult.hasContent()) {
+                List<JourneyWithStationsDTO> journeyDTOs = pagedResult.getContent().stream()
+                    .map(journey -> new JourneyWithStationsDTO(journey))
+                    .collect(Collectors.toList());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("total-pages", String.valueOf(pagedResult.getTotalPages()));
+            return new ResponseEntity<>(journeyDTOs, headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path="/journeyscount")
+    public @ResponseBody long getJourneysCount() {
+        return journeyRepository.count();
     }
 
     @CrossOrigin(origins = "http://localhost:3000")
--- src/main/java/com/citybike/repository/JourneyRepository.java
+++ src/main/java/com/citybike/repository/JourneyRepository.java
@@ -1,9 +1,10 @@
 package com.citybike.repository;
 
 import com.citybike.model.JourneyDTO;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JourneyRepository extends CrudRepository<JourneyDTO, Long> {
+public interface JourneyRepository extends JpaRepository<JourneyDTO, Long> {
 
 }
